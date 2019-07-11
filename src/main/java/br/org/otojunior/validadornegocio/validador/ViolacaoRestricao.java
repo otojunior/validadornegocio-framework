37ca57e263d80d412d89bc67c0c868e9090c04bd
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2019-07-11 09:07:02 -0300

    Implementação de equals e hashcode

--- src/main/java/br/org/otojunior/validadornegocio/validador/ViolacaoRestricao.java
+++ src/main/java/br/org/otojunior/validadornegocio/validador/ViolacaoRestricao.java
@@ -7,6 +7,7 @@ import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
@@ -20,15 +21,6 @@ import lombok.Getter;
 public class ViolacaoRestricao {
 	private String mensagem;
 	private List<String> argumentos;
-
-	/**
-	 * @param mensagem
-	 * @param argumentos
-	 */
-	private ViolacaoRestricao(String mensagem, List<String> argumentos) {
-		this.mensagem = mensagem;
-		this.argumentos = argumentos;
-	}
 	
 	/**
 	 * 
@@ -47,9 +39,38 @@ public class ViolacaoRestricao {
 	public static ViolacaoRestricao of(String mensagem, List<String> argumentos) {
 		return new ViolacaoRestricao(mensagem, argumentos);
 	}
+
+	/**
+	 * @param mensagem
+	 * @param argumentos
+	 */
+	private ViolacaoRestricao(String mensagem, List<String> argumentos) {
+		this.mensagem = mensagem;
+		this.argumentos = argumentos;
+	}
 	
 	/**
-	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		ViolacaoRestricao other = (ViolacaoRestricao) obj;
+		return Objects.equals(argumentos, other.argumentos) && Objects.equals(mensagem, other.mensagem);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(argumentos, mensagem);
+	}
+
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public String toString() {
